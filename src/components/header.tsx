626a17ed48258821d48f365ba0ce177e0d282b76
Jack Driscoll <jadr2353@colorado.edu>
2024-09-19 23:05:51 -0400

    adding css to the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,15 +2,17 @@ import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
   return (
-    <header>
-      <h1>jackdriscoll.io</h1>
-      <nav>
-        <ul>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/ideas">Ideas</Link></li>
-        </ul>
-      </nav>
+    <header className="fixed top-0 left-0 right-0 bg-white shadow-md p-4">
+      <div className="max-w-4xl mx-auto flex justify-center items-center">
+        <h1 className="text-xl font-bold mr-64">Jack Driscoll</h1>
+        <nav>
+          <ul className="flex space-x-6">
+            <li><Link to="/about" className="text-gray-700 hover:text-gray-900">About</Link></li>
+            <li><Link to="/projects" className="text-gray-700 hover:text-gray-900">Projects</Link></li>
+            <li><Link to="/ideas" className="text-gray-700 hover:text-gray-900">Ideas</Link></li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
