e2656e1c05bbdc4b106d84bc20bf4877423ab997
Jack Driscoll <jadr2353@colorado.edu>
2024-09-19 23:35:43 -0400

    adding an about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header';
-
+import About from './about.tsx';
 
 function App() {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<h1>About</h1>} />
-        <Route path="/about" element={<h1>About</h1>} />
-        <Route path="/projects" element={<h1>Projects</h1>} />
-        <Route path="/ideas" element={<h1>Ideas</h1>} />
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<About />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<h1>Projects</h1>} />
+            <Route path="/ideas" element={<h1>Ideas</h1>} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
---
+++ src/about.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div className="max-w-2xl mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-3">About Me</h1>
+      <p className="text-gray-700">
+        I'm Jack, a software engineer in New York City. 
+      </p>
+    </div>
+  );
+};
+
+export default About;
