fb31b9dd46e240e85b3856cd25c55e73195fadef
Jack Driscoll <jadr2353@colorado.edu>
2024-09-19 23:17:32 -0400

    adjusting header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
   return (
     <header className="fixed top-0 left-0 right-0 bg-white shadow-md p-4">
       <div className="max-w-4xl mx-auto flex flex-wrap justify-center items-center">
-        <h1 className="text-xl font-bold mr-48">Jack Driscoll</h1>
+        <h1 className="text-xl font-bold mr-96">Jack Driscoll</h1>
         <button 
           className="ml-auto lg:hidden"
           onClick={() => setIsMenuOpen(!isMenuOpen)}
@@ -17,7 +17,7 @@ const Header: React.FC = () => {
           </svg>
         </button>
         <nav className={`w-full lg:w-auto ${isMenuOpen ? 'block' : 'hidden'} lg:block mt-4 lg:mt-0`}>
-          <ul className="flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:space-x-6">
+          <ul className="flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:space-x-8 text-lg">
             <li><Link to="/about" className="text-gray-700 hover:text-gray-900">About</Link></li>
             <li><Link to="/projects" className="text-gray-700 hover:text-gray-900">Projects</Link></li>
             <li><Link to="/ideas" className="text-gray-700 hover:text-gray-900">Ideas</Link></li>
