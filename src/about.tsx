2e18dc94430b8f2b20e7408abd63b00ca7f8867b
Jack Driscoll <jadr2353@colorado.edu>
2024-09-20 00:17:18 -0400

    working on the about section

--- src/about.tsx
+++ src/about.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 const About: React.FC = () => {
   return (
-    <div className="max-w-2xl mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-3">About Me</h1>
+    <div className="max-w-2xl mx-auto p-4 text-left flex flex-col gap-4 mt-16">
+      <p className="text-gray-700 text-left">
+        I'm Jack. I write software and love to solve problems.
+      </p>
       <p className="text-gray-700">
-        I'm Jack, a software engineer in New York City. 
+        I currently work at Spectrum where I do a lot of ingestion, processing, and 
+        predictive analytics with viewership data. Recently I led development of an 
+        application that enabled business executives to explore and report on different metrics 
+        of viewership performance.
+      </p>
+      <p className="text-gray-700">
+        In the past I've worked 
       </p>
     </div>
   );
