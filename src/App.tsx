624b6666ce33ec0613c4c3365b3a31defafd3d2b
Jack Driscoll <jadr2353@colorado.edu>
2024-09-24 16:12:08 -0400

    working on logging, adding details for the page view/location logging

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Header from './components/header';
 import Footer from './footer.tsx';
 import About from './about.tsx';
 
+function LoggerComponent() {
+  // This compnenet logs the page view to the server, so I can track where users are navigating on the site
+  const location = useLocation();
+
+  useEffect(() => {
+    // Log the page view
+    fetch('/log-navigation', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        path: location.pathname,
+        timestamp: new Date().toISOString(),
+      }),
+    }).catch(error => console.error('Error logging navigation:', error));
+  }, [location]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen bg-light-bg">
+        <LoggerComponent />
         <Header />
         <main className="mt-16 flex-grow">
           <Routes>
