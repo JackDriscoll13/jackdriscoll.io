d394445b63699eda91ef8543e8f729536bfc5059
Jack Driscoll <jadr2353@colorado.edu>
2024-09-26 18:41:54 -0400

    ideas page!

--- jackdriscoll.io/src/ideas.tsx
+++ jackdriscoll.io/src/ideas.tsx
@@ -5,7 +5,7 @@ const Ideas: React.FC = () => {
   return (
     <div className="max-w-xl mx-auto p-4 text-left flex flex-col gap-2 text-sm mt-8 text-charcoal">
         <p> 
-        I wanted to share some ideas that ring true to me and have helped me. Some are my own, most are quotes and things I've adapted from other people. 
+        I wanted to share some ideas that ring true to me and have helped me in my journey so far. Some are my own, some are quotes I agree with that I've adapted from other people. 
         </p>
         <p>
         Subject to change!
@@ -44,6 +44,24 @@ const Ideas: React.FC = () => {
           </li>
         </ul>
         <h1 className="text-lg font-semibold italic"> Programming Thoughts </h1>
+        <ul className="list-none pl-0">
+            <li className="mb-2 flex">
+                <span className="mr-2 flex-shrink-0">-</span>
+                <span>Static Types (and docstrings) are very useful {`->`} I like working with them </span>
+            </li>
+            <li className="mb-2 flex">
+                <span className="mr-2 flex-shrink-0">-</span>
+                <span>In 90% of the cases I've encountered, functional programming makes more sense to me than OOP</span>
+            </li>
+            <li className="mb-2 flex">
+                <span className="mr-2 flex-shrink-0">-</span>
+                <span>You don't "master" programming concepts just like you don't "master" programming languages. You understand them vaguely when you read/watch about them, you go and apply them numerous times in a project and then you continuously improve on that.</span>
+            </li>
+            <li className="mb-2 flex">
+                <span className="mr-2 flex-shrink-0">-</span>
+                <span>Ship something, anything, then iterate</span>
+            </li>
+        </ul>
     </div>
   );
 };
