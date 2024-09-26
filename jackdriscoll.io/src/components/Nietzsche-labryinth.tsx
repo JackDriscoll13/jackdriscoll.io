f8398dcf67c51bfb7970803f31fd76b3995b1616
Jack Driscoll <jadr2353@colorado.edu>
2024-09-26 18:17:48 -0400

    adding Nietzsche quote

--- jackdriscoll.io/src/components/Nietzsche-labryinth.tsx
+++ jackdriscoll.io/src/components/Nietzsche-labryinth.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NietzscheQuote: React.FC = () => {
   return (
@@ -6,13 +7,21 @@ const NietzscheQuote: React.FC = () => {
       <p>
         "The most intelligent men, like the strongest, find their happiness where others would only find disaster: in the labryinth, in being hard with themselves and with others, in effort; their delight is in self-mastery; in them ascetism becomes second nature, a neccessity, an instinct. They regard a difficult task as a privilege; it is to them a recreation to play with burdens that would crush all others."
       </p>
-      <p>
+      <p className="text-right ">
         - Friedrich Nietzsche
       </p>
-      <p>
-        Yea sometimes I think about that when I'm debugging.
+      <p className="mt-12">
+        Yeah. Sometimes I think about that when I'm debugging. In the labyrinth baby.
       </p>
+      <div className="flex justify-center items-center mt-12">
+      <Link to="/ideas">
+        <button className="text-charcoal px-4 py-2 rounded-md shadow-lg">
+          {`<-`} Back to Ideas
+        </button>
+      </Link>
     </div>
+    </div>
+
   );
 };
 
