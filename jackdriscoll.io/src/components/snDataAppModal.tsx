6a38578f9ec41267aa254c1daa35cda5e078d9d8
Jack Driscoll <jadr2353@colorado.edu>
2024-11-25 15:54:34 -0500

    the problem section of the modal

--- jackdriscoll.io/src/components/snDataAppModal.tsx
+++ jackdriscoll.io/src/components/snDataAppModal.tsx
@@ -6,11 +6,23 @@ const SnDataAppModal: React.FC = () => {
         <h3 className="text-xl font-semibold mb-2">Background</h3>
         <p>
             If you are a Spectrum Cable customer, you have access to an exclusive product called Spectrum News. 
-            Spectrum News serves as a retention product for Spectrum and is a net cost for the company. 
+            Spectrum News serves as a retention product for Spectrum and operates at a net loss for the company. 
         </p>
-        <p>
-            However
+        <p className="mt-1">
+            However, many teams and analysts at Spectrum are interested in the performance of Spectrum News and its competitors, especially in the context
+            of what has been happening in the media industry over the last decade. This includes a team of analysts that operate
+            within the 2000 person Spectrum News business unit.
         </p>
+        <h3 className="text-xl font-semibold mt-4">The Problem</h3>
+        <p className="mt-1">
+            That analyst team was using alteryx and excel to perform analysis. Analysts complained that they were spending a significant amount of time running recurring reports that they felt could be automated.
+            They were interested in building a more sophisticated analytics platform to:
+        </p>
+        <ul className="list-decimal ml-6 mt-2">
+            <li>Automate Recurring Reports</li>
+            <li>Centralize data from multiple sources</li>
+            <li>Allow for quick and easy exploration of KPI data</li>
+        </ul>
         <h3 className="text-xl font-semibold mt-4">Technical Architecture</h3>
         <img 
             src="/snDataDiagram.png" 
