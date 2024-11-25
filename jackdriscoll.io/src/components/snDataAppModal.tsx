a4141b633934436f772240c99d5a42581fd41104
Jack Driscoll <jadr2353@colorado.edu>
2024-11-25 16:17:17 -0500

    techincal architetchure

--- jackdriscoll.io/src/components/snDataAppModal.tsx
+++ jackdriscoll.io/src/components/snDataAppModal.tsx
@@ -16,21 +16,28 @@ const SnDataAppModal: React.FC = () => {
         <h3 className="text-xl font-semibold mt-4">The Problem</h3>
         <p className="mt-1">
             That analyst team was using alteryx and excel to perform analysis. Analysts complained that they were spending a significant amount of time running recurring reports that they felt could be automated.
-            They were interested in building a more sophisticated analytics platform to:
+            They also complained about data quality issues and the difficulty of validating data across multiple sources. Spectrum was interested in building a more sophisticated analytics platform to:
         </p>
         <ul className="list-decimal ml-6 mt-2">
-            <li>Automate Recurring Reports</li>
+            <li>Automate Recurring Viewership Reports</li>
             <li>Centralize data from multiple sources</li>
-            <li>Allow for quick and easy exploration of KPI data</li>
+            <li>Auto validate data or flag potential issues in upstream data quality </li>
+            <li>Allow for quick and easy exploration of KPI data for analysts and executives alike</li>
         </ul>
-        <h3 className="text-xl font-semibold mt-4">Technical Architecture</h3>
+        <h3 className="text-xl font-semibold mt-4">The Solution</h3>
+        <p className="mt-1">
+            To solve these problems, I spent 6 months buildiing and iterating on a full-stack data platform in an isolated AWS enviorment.
+        </p>
+        <p className="mt-1">
+            Here's a simplified overview of the technical architecture:
+        </p>
         <img 
             src="/snDataDiagram.png" 
             alt="Project Architecture Diagram"    
             className="w-full object-contain mb-6"
         />
     <div className="space-y-4">
-      <p>Detailed project description...</p>
+      <p></p>
 
       <p>Architecture explanation...</p>
     </div>
