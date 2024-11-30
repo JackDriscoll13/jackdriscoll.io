9894c5f51d6c39f6d5ec33231f2a78caa35b3943
Jack Driscoll <jadr2353@colorado.edu>
2024-11-30 11:09:56 -0500

    changing the about section

--- jackdriscoll.io/src/about.tsx
+++ jackdriscoll.io/src/about.tsx
@@ -17,7 +17,7 @@ const About: React.FC = () => {
         </a>
         where I focus on ingestion, processing, and 
         predictive analytics with viewership data. Recently, I developed an 
-        <Link to="/projects" className="underline decoration-accent font-medium hover:text-accent cursor-pointer mx-1">
+        <Link to="/projects#sn-data-app" className="underline decoration-accent font-medium hover:text-accent cursor-pointer mx-1">
           internal data app
         </Link>
         that lets product teams explore and report out key performance metrics.
